03d46898ee43128e9425d667529c6210451a2b5a
Anbu Sampath <anbu.s27@gmail.com>
2020-02-17 19:06:15 +0530

    DataSource Completion

--- src/main/java/org/superbiz/moviefun/albums/AlbumsBean.java
+++ src/main/java/org/superbiz/moviefun/albums/AlbumsBean.java
@@ -16,21 +16,20 @@
  */
 package org.superbiz.moviefun.albums;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.criteria.CriteriaQuery;
-import java.util.List;
+
+import org.springframework.stereotype.Repository;
 
 @Repository
 public class AlbumsBean {
 
-    @PersistenceContext
+    @PersistenceContext(unitName = "albums")
     private EntityManager entityManager;
 
-    @Transactional
     public void addAlbum(Album album) {
         entityManager.persist(album);
     }
