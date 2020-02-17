1eef4aecd1f7d20f3777544f500539991744bf32
Anbu Sampath <anbu.s27@gmail.com>
2020-02-17 22:41:50 +0530

    Spring Resource abstraction

--- src/main/java/org/superbiz/moviefun/CsvUtils.java
+++ src/main/java/org/superbiz/moviefun/CsvUtils.java
@@ -1,30 +1,27 @@
 package org.superbiz.moviefun;
 
-import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.databind.ObjectReader;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import com.fasterxml.jackson.databind.MappingIterator;
+import com.fasterxml.jackson.databind.ObjectReader;
+
 public class CsvUtils {
 
     public static String readFile(String path) {
-        try {
-            Scanner scanner = new Scanner(new File(path)).useDelimiter("\\A");
 
-            if (scanner.hasNext()) {
-                return scanner.next();
-            } else {
-                return "";
-            }
-
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    	ClassLoader classLoader = CsvUtils.class.getClassLoader();
+            InputStream input = classLoader.getResourceAsStream(path);
+            try (Scanner scanner = new Scanner(input).useDelimiter("\\A")) {
+				if (scanner.hasNext()) {
+				    return scanner.next();
+				} else {
+				    return "";
+				}
+			}
     }
 
     public static <T> List<T> readFromCsv(ObjectReader objectReader, String path) {
